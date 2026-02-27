4cec32030eec369e769c1cdbbcb793e1c4027121
marialuizab11 <marialuizabsantos0@gmail.com>
2026-02-27 18:14:47 -0300

    feat: Faz room card ser clicavel para abrir a pagina de detalhes do quarto

--- frontend/src/components/room/RoomCard.tsx
+++ frontend/src/components/room/RoomCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './RoomCard.module.css';
 
 interface RoomCardProps {
+  id: number; 
   type: string;
   description: string;
   date: string;
@@ -9,23 +11,19 @@ interface RoomCardProps {
   image: string;
 }
 
-const RoomCard: React.FC<RoomCardProps> = ({ 
-    type, 
-    description, 
-    date, 
-    price, 
-    image 
-}) => {
+const RoomCard: React.FC<RoomCardProps> = ({ id, type, description, date, price, image }) => {
   return (
-    <article className={styles.roomCard}>
-      <img src={image} alt={type} className={styles.roomImage} />
-      <div className={styles.roomInfo}>
-        <h3 className={styles.roomType}>{type}</h3>
-        <p className={styles.roomDescription}>{description}</p>
-        <span className={styles.roomDate}>{date}</span>
-        <span className={styles.roomPrice}>{price}</span>
-      </div>
-    </article>
+    <Link to={`/quarto/${id}`} className={styles.cardLink}>
+      <article className={styles.roomCard}>
+        <img src={image} alt={type} className={styles.roomImage} />
+        <div className={styles.roomInfo}>
+          <h3 className={styles.roomType}>{type}</h3>
+          <p className={styles.roomDescription}>{description}</p>
+          <span className={styles.roomDate}>{date}</span>
+          <span className={styles.roomPrice}>{price}</span>
+        </div>
+      </article>
+    </Link>
   );
 };
 
